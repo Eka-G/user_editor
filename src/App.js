48bc7fd3f37e65099338fbc19f3ab6bc7e6ee4f3
Eka <ekaglazkovaa@yandex.ru>
2023-07-12 19:08:32 +0300

    build: create project structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">LLLL</header>
+    </div>
+  );
+}
+
+export default App;
